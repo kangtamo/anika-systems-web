17cd7358b2c775b215ff2b86ad4818369df48f1c
Mo Chen <chen_mo@outlook.com>
2024-10-09 10:27:08 -0400

    Refactor Cypress test command to use specPattern in cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: 'cypress/e2e/**/*.spec.js',
     baseUrl: 'https://kangtamo.github.io/anika-systems-web',  // Base URL for the tests
     setupNodeEvents(on, config) {
       // Implement node event listeners if needed
